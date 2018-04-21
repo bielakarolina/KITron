43c0ec1b57b3d106a1eef7a2165336e672f24b73
Frranek <franek-35@tlen.pl>
2018-04-21 10:56:12 +0200

    parsing

--- src/server/main/room/Room.java
+++ src/server/main/room/Room.java
@@ -3,6 +3,11 @@ package server.main.room;
 import server.main.Player;
 import server.main.PlayerState;
 
+import java.io.IOException;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.MembershipKey;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
@@ -16,12 +21,27 @@ public class Room implements Runnable{
     private boolean roomActive = false;
     private String name;
     private Timer timer;
+    private DatagramChannel channel;
+    private NetworkInterface multicastInterface = null;
+    private DatagramChannel multicastChannel = null;
+    private InetSocketAddress serverAddress = new InetSocketAddress("239.1.1.1", 5000);
 
     public Room(int width, int height, int maxPlayers, String name){
         this.board = new Board(height, width);
         this.maxPlayers = maxPlayers;
         this.name = name;
 
+    try {
+        multicastInterface = NetworkInterface.getNetworkInterfaces().nextElement();
+        multicastChannel = DatagramChannel.open(StandardProtocolFamily.INET)
+                .setOption(StandardSocketOptions.SO_REUSEADDR, true)
+                .bind(new InetSocketAddress(5000))
+                .setOption(StandardSocketOptions.IP_MULTICAST_IF, multicastInterface);
+        multicastChannel.configureBlocking(false);
+        MembershipKey groupKey = multicastChannel.join(Inet4Address.getByName("239.1.1.1"), multicastInterface);
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
     }
 
     public synchronized void join(Player player){
@@ -70,10 +90,26 @@ public class Room implements Runnable{
         @Override
         public void run() {
             //TODO
+            board.update();
+            ByteBuffer buffer = ByteBuffer.wrap(parsePlayerList().getBytes());
+            try {
+                multicastChannel.send(buffer, serverAddress);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             //process one step //BOARD
             //check collision //BOARD
             //send update //dostaje
         }
+    }
+
+    public String parsePlayerList() {
+        String s = "";
+        for(Player p : players) {
+            s = p.getId() + ";" + p.getName()+";" + p.getColor();
+            s += ";" + p.getParsedPath();
+        }
+        return s;
     }
 
     public boolean containsPlayer(Player player) {
