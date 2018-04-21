2e4b9cfe43b90818970fd17604aa55424cadd77a
kubster96 <jungiewiczjakub@gmail.com>
2018-04-21 08:43:21 +0200

    started implementing room and board

--- src/server/main/room/Board.java
+++ src/server/main/room/Board.java
@@ -1,4 +1,14 @@
 package server.main.room;
 
 public class Board {
+
+    private int[][] board;
+
+    Board(int height, int width){
+        this.board = new int[height][width];
+    }
+
+
+
+
 }
--- src/server/main/room/Room.java
+++ src/server/main/room/Room.java
@@ -4,9 +4,40 @@ import server.main.Player;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Room {
+public class Room implements Runnable{
 
     List<Player> players = new ArrayList<>();
+    private Board board;
+    private int maxPlayers;
+    private boolean roomActive = false;
+    private String name;
+
+    Room(int height, int width, int maxPlayers, String name){
+        this.board = new Board(height, width);
+        this.maxPlayers = maxPlayers;
+        this.name = name;
+
+    }
+
+    public synchronized void joinRoom(Player player){
+        players.add(player);
+    }
+
+    public boolean isRoomActive() {
+        return roomActive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void run() {
+
+        while(players.size() != maxPlayers);
+
+        //wyslanie wiadomosci do klienta ze gra sie zaczyna
 
 
+    }
 }
