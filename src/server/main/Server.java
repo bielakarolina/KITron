a2d50300f7d893dbe05e230ecc8893c782f28c5f
kubster96 <jungiewiczjakub@gmail.com>
2018-04-21 10:56:01 +0200

    ogromne zmiany 2018

--- src/server/main/Server.java
+++ src/server/main/Server.java
@@ -67,11 +67,17 @@ public class Server {
     }
 
 
-    public synchronized void addRoom(String name, int maxPeople, Player player) {
-        rooms.add(new Room(STANDARD_BOARD_WIDTH,STANDARD_BOARD_HEIGHT,maxPeople,name));
-        Room room = getRoomByName(name);
-        room.join(player);
-        System.out.println("Player " + player.getName() + " added new Room " + name + " " + maxPeople);
+    public synchronized int addRoom(String name, int maxPeople, Player player) {
+
+        if(getRoomId(name) == -1){
+            Room room = new Room(STANDARD_BOARD_WIDTH,STANDARD_BOARD_HEIGHT,maxPeople,name);
+            rooms.add(room);
+            new Thread(room).start();
+            room.join(player);
+            System.out.println("Player " + player.getName() + " added new Room " + name + " " + maxPeople);
+            return 0;
+        }
+        return 1;
     }
 
     private Room getRoomByName(String name) {
@@ -92,17 +98,27 @@ public class Server {
     }
 
 
-    public synchronized void joinRoom(Player player, int roomID){
-        rooms.get(roomID).join(player);
-        System.out.println("Player " + player.getName() + " joined Room " + roomID);
+    public synchronized int joinRoom(Player player, String roomName){
+
+        Room room = getRoomByName(roomName);
+
+        if(room != null && !room.isRoomActive())
+        {
+            room.join(player);
+            System.out.println("Player " + player.getName() + " joined Room " + roomName);
+            return 0;
+        }
+        return 1;
     }
 
-    public synchronized void leaveRoom(Player player){
+    public synchronized int leaveRoom(Player player){
         for(Room room : rooms) {
             if(room.containsPlayer(player)){
                 room.leave(player);
+                System.out.println("Player " + player.getName() + " left the room");
+                return 0;
             }
         }
-        System.out.println("Player " + player.getName() + " left the room");
+        return 1;
     }
 }
--- src/server/main/room/Room.java
+++ src/server/main/room/Room.java
@@ -3,10 +3,7 @@ package server.main.room;
 import server.main.Player;
 import server.main.PlayerState;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 
 public class Room implements Runnable{
 
@@ -21,6 +18,7 @@ public class Room implements Runnable{
         this.board = new Board(height, width);
         this.maxPlayers = maxPlayers;
         this.name = name;
+        timer = new Timer();
 
     }
 
@@ -45,14 +43,25 @@ public class Room implements Runnable{
     @Override
     public void run() {
 
-        while(players.size() != maxPlayers);
+        System.out.println("Room init");
+
+        while(players.size() != maxPlayers){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        System.out.println("Game started in: " + this.name);
 
         startGame();
         roomActive = true;
 
         //wyslanie wiadomosci do klienta ze gra sie zaczyna
 
-        timer.schedule(new processTask(), 0, 33);
+        timer.schedule(new processTask(), 0, 1000);
 
 
     }
@@ -62,6 +71,33 @@ public class Room implements Runnable{
         for(Player player : players){
             player.setPlayerState(PlayerState.PLAYING);
         }
+        putPlayersOnBoard();
+    }
+
+    private void putPlayersOnBoard() {
+        for(Player player: players){
+            player.clearPath();
+
+            List<Point> startPoints = new ArrayList<>();
+
+            int x;
+            int y;
+
+            Random r = new Random();
+
+            do{
+
+                x = r.nextInt((550 - 50) + 1) + 50;
+                y = r.nextInt((400 - 50) + 1) + 50;
+
+            } while(startPoints.contains(new Point(x, y)));
+
+
+            Point point = new Point(x, y);
+
+            player.setPosition(point);
+            player.addToPath(point);
+        }
     }
 
     private class processTask extends TimerTask{
@@ -69,6 +105,7 @@ public class Room implements Runnable{
 
         @Override
         public void run() {
+            System.out.println("Sending package");
             //TODO
             //process one step //BOARD
             //check collision //BOARD
